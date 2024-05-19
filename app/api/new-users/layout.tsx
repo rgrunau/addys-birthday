5e5593c379154e8c2954392e3d12cc573a497635
Robert Grunau <robertgrunau@hey.com>
2024-05-19 13:58:12 -0700

    more scaffolding

---
+++ app/api/new-users/layout.tsx
@@ -0,0 +1,8 @@
+
+export default function NewUsersLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
---
+++ app/api/new-users/page.tsx
@@ -0,0 +1,12 @@
+import prisma from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function NewUsersPage() {
+
+
+  return (
+    <div>
+
+    </div>
+  )
+}
