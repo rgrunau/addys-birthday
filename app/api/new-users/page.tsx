68b819ac4b513b95748704939b754722f56d99a3
Robert Grunau <robertgrunau@hey.com>
2024-05-19 20:41:27 -0700

    sign in page

--- app/api/new-users/page.tsx
+++ app/api/new-users/page.tsx
@@ -1,8 +1,33 @@
 import prisma from "@/lib/prisma";
 import { auth, currentUser } from "@clerk/nextjs/server";
 
-export default async function NewUsersPage() {
+const createNewUser = async (email) => { 
+  const user = await prisma.users.create({
+    data: {
+      email: "",
+      name: "",
+    }
+  });
+};
 
+export default async function NewUsersPage() {
+  const { userId } = await auth();
+  
+  if (userId) {
+    const user = await prisma.users.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) {
+      const currentClerkUser = await currentUser();
+      if (createNewUser !== null) {
+        
+        const newUser = await createNewUser(currentClerkUser?.email);
+      }
+    }
+  }
 
   return (
     <div>
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,13 @@
+import { inter } from "@/constants/fonts";
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp fallbackRedirectUrl="new-users" path="/sign-up" />;
+  return (
+    <div className={`${inter.className} flex flex-col items-center justify-center p-24`}>
+      <div>
+        <SignUp fallbackRedirectUrl="new-users" path="/sign-up" />;
+      </div>
+    </div>
+  )
+  
 }
