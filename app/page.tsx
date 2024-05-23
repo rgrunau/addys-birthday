0ee3bb94907f76d3d9cd91075d68d8ca93e2e88b
Robert Grunau <robertgrunau@hey.com>
2024-05-22 22:06:31 -0700

    more quotes

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-between">
       
-      <section className="flex flex-col z-10 w-full h-full relative items-center">
+      <section className="flex flex-col z-10 w-full h-screen relative items-center">
         <header className="w-full h-16 bg-transparent flex items-center justify-between px-4 z-10">
-          <h1 className="text-2xl text-black font-bold">Addy's Birthday</h1>
+          <h1 className="text-2xl text-black font-bold">{"Addy's Birthday"}</h1>
         </header>
         <div className="w-full max-w-4xl my-28 z-10 flex items-center justify-start bg-slate-400/45 px-6 py-8 rounded-lg ">
           <div className="w-full">
@@ -20,7 +20,7 @@ export default function Home() {
           <Image
             src={CoverImage}
             alt="Addy's Birthday"
-            className="object-contain"
+            className="object-cover w-full h-screen"
           />
         </div>
       </section>
