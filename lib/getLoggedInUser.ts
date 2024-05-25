bf3d6bbe7988e9f5100c5bd99fa4679309b10b0d
Robert Grunau <robertgrunau@hey.com>
2024-05-25 08:41:18 -0700

    fix prisma not defined

--- lib/getLoggedInUser.ts
+++ lib/getLoggedInUser.ts
@@ -1,12 +1,12 @@
 import { auth } from "@clerk/nextjs/server";
-
+import prisma from "./prisma";
 
 export const getCurrentUserByClerkId = async () => { 
   const { userId } = await auth();
   if (!userId) {
     throw new Error('User not found');
   }
-  const dbUser = await prisma.users.findFirst({
+  const dbUser = await prisma?.users?.findFirst({
     where: {
       clerkId: userId
     }
