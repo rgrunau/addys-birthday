44ae4c77e7c015c458a22ea09d24877b6bc6a4ea
Robert Grunau <robertgrunau@hey.com>
2024-05-25 08:23:22 -0700

    try code from github

--- app/api/create-event/route.ts
+++ app/api/create-event/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
 import { getCurrentUserByClerkId } from "@/lib/getLoggedInUser";
+import prisma from "@/lib/prisma";
 
 export async function POST(req: Request){
   try {
